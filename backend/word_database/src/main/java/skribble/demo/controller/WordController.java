6ee5b024ee112cc646d7ebbf800f7cb10a4d4717
tvgabriel <tv2000gabriel@gmail.com>
2025-01-11 17:32:03 +0200

    Added word database with CRUD operations

---
+++ backend/word_database/src/main/java/skribble/demo/controller/WordController.java
@@ -0,0 +1,53 @@
+package skribble.demo.controller;
+// jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import skribble.demo.entity.Word;
+import skribble.demo.service.WordService;
+
+import java.util.List;
+
+@RestController
+public class WordController {
+    @Autowired private WordService wordService;
+
+    @PostMapping("/words")
+    public Word saveWord(
+            @RequestBody Word word)
+    {
+        return wordService.saveWord(word);
+    }
+
+    // Read operation
+    @GetMapping("/words")
+    public List<Word> fetchWordsList()
+    {
+        return wordService.fetchWordList();
+    }
+
+    @GetMapping("/words/{count}")
+    public List<Word> fetchRandomWordList(@PathVariable("count") int count)
+    {
+        return wordService.fetchRandomWordList(count);
+    }
+
+    // Update operation
+    @PutMapping("/words/{id}")
+    public Word
+    updateWord(@RequestBody Word word,
+                     @PathVariable("id") Long wordId)
+    {
+        return wordService.updateWord(
+                word, wordId);
+    }
+
+    // Delete operation
+    @DeleteMapping("/words/{id}")
+    public String deleteWordById(@PathVariable("id")
+                                       Long wordId)
+    {
+        wordService.deleteWordById(
+                wordId);
+        return "Deleted Successfully";
+    }
+}
